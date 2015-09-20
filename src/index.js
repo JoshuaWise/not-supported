4a592c3d5f3432d85b1bb6cab4025a010aa094bd
Joshua Wise <josh@joshuawise.ninja>
2015-09-20 14:53:34 -0400

    added tests to index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,34 @@ Modernizr.addTest('cssbackgroundoptions', require('./custom-detects/css/backgrou
 Modernizr.addTest('csscalc', require('./custom-detects/css/calc'));
 Modernizr.addTest('requestanimationframe', require('./custom-detects/requestanimationframe'));
 Modernizr.addTest('matchmedia', require('./custom-detects/matchmedia'));
+
+var modern = !!(Modernizr.es5
+	&& Modernizr.cssremunit
+	&& Modernizr.cssvwunit
+	&& Modernizr.cssvhunit
+	&& Modernizr.csscalc
+	&& Modernizr.cssboxsizing
+	&& Modernizr.csspointerevents
+	&& Modernizr.userselect
+	&& Modernizr.csstransitions
+	&& Modernizr.cssanimations
+	&& Modernizr.csstransforms3d
+	&& Modernizr.csslineargradient
+	&& Modernizr.cssbackgroundoptions
+	&& Modernizr.flexbox
+	&& Modernizr.flexwrap
+	&& Modernizr.typedarrays
+	&& Modernizr.blobconstructor
+	&& Modernizr.bloburls
+	&& Modernizr.websockets
+	&& Modernizr.websocketsbinary
+	&& Modernizr.webworkers
+	&& Modernizr.canvastext
+	&& Modernizr.scriptdefer
+	&& Modernizr.scriptasync
+	&& Modernizr.audio
+	&& Modernizr.video
+	&& Modernizr.inlinesvg
+	&& Modernizr.requestanimationframe
+	&& Modernizr.matchmedia
+);
