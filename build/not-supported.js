abba204878b9e9f9144b193e36c15bf24c87feb0
Joshua Wise <josh@joshuawise.ninja>
2015-10-06 17:39:54 -0400

    updated modernizr

--- build/not-supported.js
+++ build/not-supported.js
@@ -1,13 +1,12 @@
 /*!
- * modernizr v3.0.0
- * Build http://modernizr.com/download?-audio-blobconstructor-canvastext-classlist-cssanimations-csstransforms3d-cssvhunit-cssvwunit-dataset-es5-flexbox-flexwrap-history-inlinesvg-progressbar_meter-scriptasync-scriptdefer-typedarrays-unknownelements-userselect-video-websockets-websocketsbinary-webworkers-addtest-atrule-domprefixes-hasevent-load-mq-prefixed-prefixedcss-prefixes-testallprops-testprop-teststyles-dontmin
+ * modernizr v3.1.0
+ * Build http://modernizr.com/download?-audio-blobconstructor-canvastext-classlist-cssanimations-csstransforms3d-cssvhunit-cssvwunit-dataset-es5-flexbox-flexwrap-history-inlinesvg-progressbar_meter-scriptasync-scriptdefer-typedarrays-unknownelements-userselect-video-websockets-websocketsbinary-webworkers-addtest-atrule-domprefixes-hasevent-load-mq-prefixed-prefixedcss-prefixedcssvalue-prefixes-testallprops-testprop-teststyles-dontmin
  *
  * Copyright (c)
  *  Faruk Ates
  *  Paul Irish
  *  Alex Sexton
  *  Ryan Seddon
- *  Alexander Farkas
  *  Patrick Kettner
  *  Stu Cox
  *  Richard Herrera
@@ -37,7 +36,7 @@
 
   var ModernizrProto = {
     // The current version, dummy
-    _version: '3.0.0',
+    _version: '3.1.0',
 
     // Any settings that don't work as separate modules
     // can go in here as configuration.
@@ -1347,6 +1346,56 @@
     var prefixedProp = prefixed(prop);
     return prefixedProp && domToCSS(prefixedProp);
   };
+  
+
+  /**
+   * prefixedCSSValue is a way test for prefixed css properties (e.g. display: -webkit-flex)
+   *
+   * @memberof Modernizr
+   * @name Modernizr.prefixedCSSValue
+   * @optionName Modernizr.prefixedCSSValue()
+   * @optionProp prefixedCSSValue
+   * @access public
+   * @function prefixedCSSValue
+   * @param {string} prop - String name of the property to test for
+   * @param {string} value - String value of the non prefixed version of the value you want to test for
+   * @returns {string|false} The string representing the (possibly prefixed)
+   * valid version of the property, or `false` when it is unsupported.
+   * @example
+   *
+   * `Modernizr.prefixedCSSValue` is a way test for prefixed css properties (e.g. display: -webkit-flex)
+   *
+   * ```js
+   * Modernizr.prefixedCSSValue('background', 'linear-gradient(left, red, red)')
+   * ```
+   *
+   */
+
+  var prefixedCSSValue = function(prop, value) {
+    var result = false;
+    var elem = createElement('div');
+    var style = elem.style;
+
+    if (prop in style) {
+      var i = domPrefixes.length;
+
+      style[prop] = value;
+      result = style[prop];
+
+      while (i-- && !result) {
+        style[prop] = '-' + domPrefixes[i] + '-' + value;
+        result = style[prop];
+      }
+    }
+
+    if (result === '') {
+      result = false;
+    }
+
+    return result;
+  };
+
+  ModernizrProto.prefixedCSSValue = prefixedCSSValue;
   
 
   /**
