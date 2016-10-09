115450b06f2803cb80a0a27acab37b58cf6f0e25
Joshua Wise <joshuathomaswise@gmail.com>
2016-10-09 04:19:18 -0400

    fixed reference error

--- src/custom-detects/css/vhunit.js
+++ src/custom-detects/css/vhunit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = testStyles('#modernizr { height: 50vh; }', function (el) {
+module.exports = Modernizr.testStyles('#modernizr { height: 50vh; }', function (el) {
 	var height = parseInt(window.innerHeight / 2, 10);
 	var compStyle = parseInt(
 		(window.getComputedStyle
